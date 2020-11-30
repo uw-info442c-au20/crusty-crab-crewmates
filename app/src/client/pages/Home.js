8017c68568d90afac5ff875149a44c6f1de33367
Aaron Zhao <aaronzhao@Aarons-MacBook-Pro.local>
2020-11-30 10:52:21 -0800

    add backgronds to homepage

--- app/src/client/pages/Home.js
+++ app/src/client/pages/Home.js
@@ -13,26 +13,36 @@ export default class Home extends Component {
     render() {
         return (
             <div className="bg" style={{
-                backgroundColor: "green",
-                height: "100%"
+                backgroundImage: `url(${require("../imgs/recycle-bg1.PNG")})`,
+                backgroundPosition: "50% 5%",
+                backgroundRepeat: "repeat",
             }}>
-                <h1 className="text-center">Sort</h1>
-                <div style={{
-                    position: "relative",
-                    zIndex: "1",
-                    backgroundColor: "white",
-                    borderRadius: "32px"
-                }} className="card-deck w-75 h-75 ml-auto mr-auto">
-                    <p style={{
-                        color: "#3C9471",
-                        fontSize: "27px"
-                    }} className="text-left ml-3">Select a plastic to learn more</p>
+                <div className="bg" style={{
+                    backgroundImage: `url(${require("../imgs/recycle-bg2.PNG")})`,
+                    backgroundPosition: "50% 95%",
+                    backgroundRepeat: "repeat",
+                }}>
+                    <h1 className="text-center">Sort</h1>
                     <div style={{
-                        display: 'grid',
-                        justifyContent: "space-evenly",
-                        gridTemplateColumns: "auto auto auto" 
-                    }}>
-                        {this.state.data.map(d => <PlasticCard key={d} number={d} />)}
+                        position: "relative",
+                        zIndex: "1",
+                        backgroundColor: "white",
+                        borderRadius: "32px",
+                        borderStyle: "solid",
+                        borderWidth: "0.5px",
+                        borderColor: "grey"
+                    }} className="card-deck w-75 h-75 ml-auto mr-auto">
+                        <p style={{
+                            color: "#3C9471",
+                            fontSize: "27px"
+                        }} className="text-left ml-3">Select a plastic to learn more</p>
+                        <div style={{
+                            display: 'grid',
+                            justifyContent: "space-evenly",
+                            gridTemplateColumns: "auto auto auto" 
+                        }}>
+                            {this.state.data.map(d => <PlasticCard key={d} number={d} />)}
+                        </div>
                     </div>
                 </div>
             </div>
