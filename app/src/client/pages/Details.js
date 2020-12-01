8218103fff76492e8063da0dec467ff3e8c76810
Alejandro Huante <alejandrohuante@Alejandros-MacBook-Pro-2.local>
2020-12-01 12:00:35 -0800

    Added background to details page

--- app/src/client/pages/Details.js
+++ app/src/client/pages/Details.js
@@ -1,11 +1,30 @@
-import React from 'react';
 import { useParams } from 'react-router';
 
 export default function Details() {
     const { id } = useParams();
 
+    const number = {};
+    for (let i = 1; i < 10; i++) {
+        number[i] = "0" + i;
+    }
+
     return (
-        <h1>wow this is recycle number {id}</h1>
+        <div className="bg" style={{
+            backgroundImage: `url(${require("../imgs/recycle-homepage1.png")})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "65% 250%",
+            backgroundSize: "700px"
+        }}>
+            <div className="bg" style={{
+                backgroundImage: `url(${require("../imgs/pvc-tube.png")})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "15% 20%"
+            }}>
+                <h1 className="ml-3" style={{
+                    color: "#F7F7F7",
+                    fontSize: "70px"
+                }}>{number[id]}</h1>
+            </div>
+        </div>
     )
 }
-
