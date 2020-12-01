4cbbc17c02745e6630dbbb4a12380cc3d7088273
Alejandro Huante <alejandrohuante@Alejandros-MacBook-Pro-2.local>
2020-12-01 11:55:02 -0800

    Add link to the number buttons

--- app/src/client/components/PlasticCard.js
+++ app/src/client/components/PlasticCard.js
@@ -1,33 +1,35 @@
-import React, { Component } from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export default class PlasticCard extends Component {
     render() {
         return (
-            <Card style={{
-                // box dimenstions
-                width: '66px',
-                height: "66px",
-                // border styles
-                borderRadius: "20px",
-                borderStyle: "solid",
-                borderColor: "#3C9471",
-                borderWidth: "3px",
-                // background image styles
-                backgroundImage: `url(${require("../imgs/recycle.png")})`,
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
-            }}>
-                <Card.Body style={{
-                    verticalAlign: "middle",
-                    padding: "0 0 0 0" // get rid of default padding on card body
+            <Link to={"/details/" + this.props.number}>
+                <Card style={{
+                    // box dimenstions
+                    width: '66px',
+                    height: "66px",
+                    // border styles
+                    borderRadius: "20px",
+                    borderStyle: "solid",
+                    borderColor: "#3C9471",
+                    borderWidth: "3px",
+                    // background image styles
+                    backgroundImage: `url(${require("../imgs/recycle.png")})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
                 }}>
-                    <Card.Title className="text-center" style={{
-                        fontSize: "43px",
-                        color: "#16605A"
-                    }}>{this.props.number}</Card.Title>
-                </Card.Body>
-            </Card>
+                    <Card.Body style={{
+                        verticalAlign: "middle",
+                        padding: "0 0 0 0" // get rid of default padding on card body
+                    }}>
+                        <Card.Title className="text-center" style={{
+                            fontSize: "43px",
+                            color: "#16605A"
+                        }}>{this.props.number}</Card.Title>
+                    </Card.Body>
+                </Card>
+            </Link>
         )
     }
 }
