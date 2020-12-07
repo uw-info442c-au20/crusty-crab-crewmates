4a418e3cca897bb60c4ab2ab744d0637c71a3e41
badpickle149 <kevinweng95@gmail.com>
2020-12-06 19:13:03 -0800

    Add exit button to pop up

--- app/src/client/components/DetailsModal.js
+++ app/src/client/components/DetailsModal.js
@@ -27,36 +27,35 @@ export default class DetailsModal extends Component {
         return (
             <div className={showHideClassName}>
                 <section className="modal-main">
-                    <div style={{
-                        width: "100vw",
-                        backgroundImage: `url(${require("../imgs/recycle-homepage2.png")})`,
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "100vw",
-                        backgroundPosition: "-10vw -20vh"
-                    }}>
-                        <div id="details-container" style={{
-                            marginLeft: "15vw",
-                            marginTop: "10vh",
-                            height: "100vh",
-                        }} className="details d-flex flex-row">
-                            <div style={{
-                                marginTop: "30vh"
-                            }}>
-                                <h2 style={{
-                                    color: "#18796A"
-                                }}>{datum.PlasticName + " (" + datum.PlasticAbv + ")"}</h2>
-                                <p style={{
-                                    width: "40vw"
-                                }}>{datum.HowToRecycle}</p>
-                            </div>
-                            <img style={{
-                                maxWidth: "20vw",
-                                maxHeight: "20vw",
-                                width: "auto",
-                                height: "auto",
-                                marginTop: "25vh"
-                            }} alt="pvc tube" src={require(`../imgs/recycle-img-${number}.png`)} />
+                    <p onClick={this.props.onClose} style={{
+                        position: "fixed",
+                        left: "5%",
+                        top: "5%",
+                        fontSize: "30px",
+                        cursor: "pointer"
+                    }}>X</p>
+                    <div id="details-container" style={{
+                        marginLeft: "15vw",
+                        marginTop: "10vh",
+                        height: "100vh",
+                    }} className="details d-flex flex-row">
+                        <div style={{
+                            marginTop: "30vh"
+                        }}>
+                            <h2 style={{
+                                color: "#18796A"
+                            }}>{datum.PlasticName + " (" + datum.PlasticAbv + ")"}</h2>
+                            <p style={{
+                                width: "40vw"
+                            }}>{datum.HowToRecycle}</p>
                         </div>
+                        <img style={{
+                            maxWidth: "20vw",
+                            maxHeight: "20vw",
+                            width: "auto",
+                            height: "auto",
+                            marginTop: "25vh"
+                        }} alt="pvc tube" src={require(`../imgs/recycle-img-${number}.png`)} />
                     </div>
                 </section>
             </div>
